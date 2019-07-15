f600eff4f9d2116940fbe8f2bee53312773a394e
Vanya Dimitrova <vanyad@gmail.com>
2019-07-15 08:48:43 +0300

    Test commit

---
+++ InteriorDesign/Models/InputModels/ProjectCalculatorInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace InteriorDesign.Models.InputModels
+{
+    public class ProjectCalculatorInputModel
+    {
+        public string Property { get; set; }
+
+        public string Project { get; set; }
+
+        public int NumberOfBedrooms { get; set; }
+
+        public decimal NumberOfBathrooms { get; set; }
+
+        public decimal HousingArea { get; set; }
+
+        public string Description { get; set; }
+    }
+}
