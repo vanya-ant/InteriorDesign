b669186a0d9dbdf539fd7f395f5034572ee00dd3
Vanya Dimitrova <vanyad@gmail.com>
2019-07-18 22:56:39 +0300

    SpecifiedProjectFileCloudinaryProperties

--- InteriorDesign/Data/InteriorDesign.Data.Models/ProjectFile.cs
+++ InteriorDesign/Data/InteriorDesign.Data.Models/ProjectFile.cs
@@ -3,14 +3,21 @@
     using System;
 
     using InteriorDesign.Data.Common.Models;
+    using Microsoft.AspNetCore.Http;
 
     public class ProjectFile : BaseModel<string>
     {
+        public string Utl { get; set; }
+
+        public IFormFile File { get; set; }
+
         public DateTime AddedOn { get; set; }
 
         public bool IsApproved { get; set; }
 
         public bool IsPublic { get; set; }
+
+        public string PublicId { get; set; }
 
         public string ProjectId { get; set; }
 
--- InteriorDesign/Web/InteriorDesign.Web/Controllers/DesignerController.cs
+++ InteriorDesign/Web/InteriorDesign.Web/Controllers/DesignerController.cs
@@ -1,16 +1,12 @@
 ﻿namespace InteriorDesign.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
 
     public class DesignerController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            return this.View();
         }
     }
 }
